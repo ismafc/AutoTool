9000968e0ecc11be960ec6007c37b48851c75a3b
Ismael Flores <iflores@circontrol.com>
2024-04-08 17:31:22 +0200

    Add solution for CodeChef problem "Cleaning Up" (CLEANUP)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -5371,6 +5371,72 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Cleaning Up
+     * 
+     * After a long and successful day of preparing food for the banquet, it is time to clean up. 
+     * There is a list of n jobs to do before the kitchen can be closed for the night. 
+     * These jobs are indexed from 1 to n.
+     * Most of the cooks have already left and only the Chef and his assistant are left to clean up. 
+     * Thankfully, some of the cooks took care of some of the jobs before they left so only a subset of the n jobs remain. 
+     * The Chef and his assistant divide up the remaining jobs in the following manner. 
+     * The Chef takes the unfinished job with least index, 
+     * the assistant takes the unfinished job with the second least index, 
+     * the Chef takes the unfinished job with the third least index, etc. 
+     * That is, if the unfinished jobs were listed in increasing order of their index 
+     * then the Chef would take every other one starting with the first job in the list 
+     * and the assistant would take every other one starting with the second job on in the list.
+     * The cooks logged which jobs they finished before they left. 
+     * Unfortunately, these jobs were not recorded in any particular order. 
+     * Given an unsorted list of finished jobs, 
+     * you are to determine which jobs the Chef must complete 
+     * and which jobs his assitant must complete before closing the kitchen for the evening.
+     * 
+     * Input:
+     * The first line contains a single integer T <= 50 indicating the number of test cases to follow. 
+     * Each test case consists of two lines. 
+     * The first line contains two numbers n, m satisfying 1 <= m <= n <= 1000. 
+     * Here, n is the total number of jobs that must be completed before closing 
+     * and m is the number of jobs that have already been completed. 
+     * The second line contains a list of m distinct integers between 1 and n. 
+     * These are the indices of the jobs that have already been completed. 
+     * Consecutive integers are separated by a single space.
+     * 
+     * Output:
+     * The output for each test case consists of two lines. 
+     * The first line is a list of the indices of the jobs assigned to the Chef.
+     * The second line is a list of the indices of the jobs assigned to his assistant.
+     * Both lists must appear in increasing order of indices and consecutive integers should be separated by a single space.
+     * If either the Chef or the assistant is not assigned any jobs, 
+     * then their corresponding line should be blank -1.
+     */
+    public static void solveCodeChefCLEANUP() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            long m = scn.nextLong();
+            long n = scn.nextLong();
+            HashMap<Long, Boolean> jobs = new HashMap<>();
+            while (n-- > 0)
+                jobs.put(scn.nextLong(), true);
+            String chef = "";
+            String assistant = "";
+            long j = 0;
+            for (long i = 1; i < m + 1; i++) {
+                if (!jobs.containsKey(i)) {
+                    if (j % 2 == 0)
+                        chef += i + " ";
+                    else
+                        assistant += i + " ";
+                    j++; 
+                }
+            }
+            System.out.println(chef.length() > 0 ? chef.substring(0, chef.length() - 1) : "-1");
+            System.out.println(assistant.length() > 0 ? assistant.substring(0, assistant.length() - 1) : "-1");
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
