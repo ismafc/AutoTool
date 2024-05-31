75e1a18794579493dfc6cc8fcc7d2b19274b92a2
Ismael Flores <ismafc@gmail.com>
2024-05-31 23:19:23 +0200

    Add solution for CodeChef problem 'Who Makes P1' (WHOMAKESP1)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -6545,6 +6545,36 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Who Makes P1
+     *
+     * Tyro and Dom are fighting over who makes the first problem of the contest. 
+     * Both of them are very lazy so they don't want to make it.
+     * Tyro has a patience level of A, meaning he will only be convinced on the Ath time when Dom asks him to make the problem.
+     * Dom, on the other hand, has decided to convince Tyro at most B times. 
+     * If Tyro is not convinced by the Bth time, Dom will proceed to make it himself.
+     * Given the values of A and B, find out who will end up making the problem.
+     *
+     * Input Format:
+     * The first and only line of input contains 2 integers, A and B - 
+     * Tyro's patience level, and the number of times Dom tries to convince Tyro respectively.
+     * 
+     * Output Format:
+     * For each test case, output on a new line, Dom or Tyro corresponding to who ends up making the problem.
+     * You can print each character in uppercase or lowercase. 
+     * For example, the strings DOM, dom, Dom, and dOM are considered identical.
+     *
+     * Constraints:
+     * 1 <= A, B <= 10
+     */
+    public static void solveCodeChefWHOMAKESP1() {
+        Scanner scn = new Scanner(System.in);
+        long A = scn.nextLong();
+        long B = scn.nextLong();
+        System.out.println(B >= A ? "Tyro" : "Dom");
+        scn.close();
+    }
+    
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
