9a20588b87a1bd100f840f11b7be02cac8a28f35
Ismael Flores <iflores@circontrol.com>
2024-06-10 08:47:49 +0200

    feat: Implement solution for CodeChef problem "Election Hopes" (ELHP)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -6659,6 +6659,35 @@ public class CodeChef {
         }
         scn.close();
     }
+
+    /*
+     * Problem: Election Hopes
+     * 
+     * The elections in Chefland have concluded, and the results are in!
+     * Chef received X votes, and his rival Chefu received Y.
+     * Chef thinks he dominated the election if and only if he received at least double the number of votes Chefu received.
+     * Did Chef dominate the election?
+     * 
+     * Input Format:
+     * The only line of input contains two space-separated integers X and Y - 
+     * the number of votes received by Chef and Chefu, respectively.
+     * 
+     * Output Format:
+     * Print a single line containing the answer: either "Yes" or "No" (without quotes), 
+     * depending on whether Chef dominated the election or not.
+     * Each letter of the output may be printed in either uppercase or lowercase, 
+     * i.e, the strings No, no, NO, and nO will all be treated as equivalent.
+     * 
+     * Constraints:
+     * 1 <= X, Y <= 100
+     */
+    public static void solveCodeChefELHP() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong();
+        long Y = scn.nextLong();
+        System.out.println(X >= Y * 2 ? "Yes" : "No");
+        scn.close();
+    }
     
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
