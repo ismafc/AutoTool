e980647dc0ff946ec5fda5f595169f80f7414fb9
Ismael Flores <iflores@circontrol.com>
2024-06-18 12:44:03 +0200

    feat: Add solution for CodeChef problem "Mask Policy" (MASKPOL)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -7019,6 +7019,42 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Mask Policy
+     * 
+     * A city has been infected by a contagious virus.
+     * In a survey, it was found that A out of the N people living in the city are currently infected. 
+     * It has been observed that the only way for a person to get infected is 
+     * if he comes in contact with an already infected person, and both of them are NOT wearing a mask.
+     * The mayor of the city wants to make a new Mask Policy and find out the minimum number of people 
+     * that will be required to wear a mask to avoid the further spread of the virus. 
+     * Help the mayor in finding this number.
+     * Note: The only aim of the mayor is to stop virus spread, not to mask every infected person.
+     * 
+     * Input Format:
+     * The first line contains T - number of test cases. Then the test cases follow.
+     * The first and only line of each test case contains two integers N and A - 
+     * the total number of people living in the city and the number of people already affected by the virus respectively.
+     * 
+     * Output Format:
+     * For each test case, output the minimum number of people that will be required to wear a mask so as to curb the virus spread.
+     * 
+     * Constraints:
+     * 1 <= T <= 10^5
+     * 2 <= N <= 400
+     * 1 <= A < N
+     */
+    public static void solveCodeChefMASKPOL() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong(); 
+        while (T-- > 0) {
+            long N = scn.nextLong(); 
+            long A = scn.nextLong(); 
+            System.out.println(Math.min(N - A, A));
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
