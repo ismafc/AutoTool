443977b1b9f98cdea2a0466dc66c4604b94a2289
Ismael Flores <iflores@circontrol.com>
2024-06-19 17:24:48 +0200

    feat: Add solution for CodeChef problem "Chef and Street Food" (STFOOD)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -7095,6 +7095,52 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Chef and Street Food
+     * 
+     * in Chefland, there is a very famous street where N types of street food (numbered 1 through N) are offered. 
+     * For each valid i, there are Si stores that offer food of the ith type, 
+     * the price of one piece of food of this type is Vi (the same in each of these stores) 
+     * and each day, Pi people come to buy it; each of these people wants to buy one piece of food of the ith type.
+     * Chef is planning to open a new store at this street, where he would offer food of one of these NN types. 
+     * Chef assumes that the people who want to buy the type of food he'd offer will split equally among all stores that offer it, 
+     * and if this is impossible, i.e. the number of these people pp is not divisible by the number of these stores s, 
+     * then only floor(p/s) people will buy food from Chef. Chef wants to maximise his daily profit. 
+     * Help Chef choose which type of food to offer and find the maximum daily profit he can make.
+     * 
+     * Input:
+     * The first line of the input contains a single integer T denoting the number of test cases. 
+     * The description of T test cases follows. 
+     * The first line of each test case contains a single integer N.
+     * N lines follow. For each i (1 <= i <= N), the ith of these lines contains three space-separated integers Si, Pi and Vi.
+     * 
+     * Output:
+     * For each test case, print a single line containing one integer - the maximum profit.
+     * 
+     * Constraints:
+     * 1 <= T <= 100
+     * 1 <= N <= 100
+     * 1 <= Si, Vi, Pi <= 10000 for each valid i
+     */
+    public static void solveCodeChefSTFOOD() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong(); 
+        while (T-- > 0) {
+            long N = scn.nextLong(); 
+            long maxProfit = 0;
+            for (long i = 0; i < N; i++) {
+                long Si = scn.nextLong();
+                long Pi = scn.nextLong();
+                long Vi = scn.nextLong();
+                long newProfit = (Pi / (Si + 1)) * Vi;
+                if (newProfit > maxProfit)
+                    maxProfit = newProfit;
+            }
+            System.out.println(maxProfit);
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
