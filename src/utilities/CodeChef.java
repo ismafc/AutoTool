e8ba2128ad96900414ed9db51e4eac7b9bf8d31c
Ismael Flores <iflores@circontrol.com>
2024-07-23 09:29:27 +0200

    feat: Implement solution for CodeChef problem 'Chef and Coupon' (COUPON2)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -6830,7 +6830,7 @@ public class CodeChef {
      * 
      * An input string SS of length N is transferred through the network using a special protocol. 
      * The protocol can send the string through a series of operations. 
-     * In one operation, we can choose a lowercase english alphabet CC and do one of the following:
+     * In one operation, we can choose a lowercase english alphabet C and do one of the following:
      * Transfer 1 copy of C through the network.
      * Transfer 2 copies of C through the network.
      * Each of the above transfers take 1 unit of time.
@@ -7655,6 +7655,55 @@ public class CodeChef {
         scn.close();
     }
     
+    /*
+     * Problem: Chef and Coupon
+     * 
+     * Chef wants to order food from a food delivery app. 
+     * He wishes to order once today, and buy three items costing A1, A2 and A3 rupees, respectively. 
+     * He'll also order once tomorrow, when he'll buy three items costing B1, B2 and B3 rupees, respectively. 
+     * There is an additional delivery charge of rupees D for each order.
+     * He notices that there is a coupon on sale, which costs rupees C. 
+     * If he buys that coupon, the delivery charges on any day, on an order of rupees 150 or more shall be waived 
+     * (that is, the D rupees will not be added, if the sum of the costs of the items is >= 150).
+     * Note that Chef is ordering the three items together on each day, 
+     * so the delivery charge is charged only once each day. 
+     * Also, note that it's only needed to buy the coupon once to avail the delivery fee waiver on both days.
+     * Should Chef buy the coupon? 
+     * Note that Chef shall buy the coupon only if it costs him strictly less than what it costs him without the coupon, in total.
+     * 
+     * Input:
+     * The first line of the input contains a single integer T, denoting the number of test cases.
+     * The first line of each test case contains two space-separated integers D and C, 
+     * denoting the delivery charge, and the price of the coupon, respectively.
+     * The second line of each test case contains three space-separated integers A1, A2 and A3, 
+     * denoting the prices of the food items that Chef plans to order on Day 1, respectively.
+     * The third line of each test case contains three space-separated integers B1, B2 and B3, 
+     * denoting the prices of the food items that Chef plans to order on Day 2, respectively.
+     * 
+     * Output: 
+     * For each test case, output YES if Chef should buy the coupon, and NO otherwise, in a separate line.
+     * 
+     * Constraints:
+     * 1 <= T <= 10^4
+     * 1 <= D, C <= 100
+     * 1 <= A1, A2, A3 <= 100
+     * 1 <= B1, B2, B3 <= 100
+     */
+    public static void solveCodeChefCOUPON2() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong(); 
+        while (T-- > 0) {
+            long D = scn.nextLong();
+            long C = scn.nextLong();
+            long A = scn.nextLong() + scn.nextLong() + scn.nextLong();
+            long B = scn.nextLong() + scn.nextLong() + scn.nextLong();
+            long P1 = 2 * D + A + B;
+            long P2 = C + (A >= 150 ? A : A + D) + (B >= 150 ? B : B + D);
+            System.out.println(P2 < P1 ? "YES" : "NO");
+        }
+        scn.close();
+    }
+
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
