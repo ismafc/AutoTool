c02e7b77156760787dfd5b9603bfd3aa0678ac4e
Ismael Flores <ismafc@gmail.com>
2024-07-27 13:26:18 +0200

    feat: Add solution for CodeChef problem 'Capital Gain Tax' (CGT)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -7757,6 +7757,39 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Capital Gain Tax
+     *
+     * The annual budget for ChefLand has been announced and people are concerned about the modification in capital gain tax.
+     * Given that the capital gain tax changed from X%X% to Y%Y%, find whether it has INCREASED, DECREASED, or remained the SAME.
+     *
+     * Input Format:
+     * The first and only line of input consists of two space-separated integers X and Y denoting the previous and new value of capital gain tax.
+     *
+     * Output Format:
+     * Output on a new line:
+     * -> INCREASED, if the capital gain tax has increased;
+     * -> DECREASED, if the capital gain tax has decreased;
+     * -> SAME, if the capital gain tax remained the same.
+     * You may print each character in uppercase or lowercase. 
+     * For example, the strings SAME, same, Same, and sAmE would be considered identical.
+     *
+     * Constraints:
+     * 10 <= X, Y <= 30
+     */
+    public static void solveCodeChefTCG() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong();
+        long Y = scn.nextLong();
+        if (X < Y)
+            System.out.println("INCREASED");
+        else if (X > Y)
+            System.out.println("DECREASED");
+        else
+            System.out.println("SAME");
+        scn.close();
+    }
+    
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
