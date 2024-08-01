8a3a4ab20a9f8a7c592cd1d1005a45a18471e7ce
Ismael Flores <iflores@circontrol.com>
2024-08-01 16:02:30 +0200

    feat: Add solution for CodeChef problem 'Volume Comparison' (CABLE)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -7899,6 +7899,47 @@ public class CodeChef {
         }
         scn.close();
     }
+
+    /*
+     * Problem: Volume Comparison
+     * 
+     * Alice has two objects:
+     * A cuboid with length A units, height B units, and width C units.
+     * A cube with an edge length of X units.
+     * Alice wants to know which of the two objects has a larger volume, or if their volumes are equal.
+     * 
+     * Input Format:
+     * The first and only line of input will contain four space-separated integers A, B, C and X, 
+     * the length of the cuboid, the width of the cuboid, the height of the cuboid, and the length of the edge of the cube.
+     * 
+     * Output Format:
+     * Print a single line containing the string:
+     * "Cuboid", if the volume of the cuboid is greater than the volume of the cube.
+     * "Cube", if the volume of the cube is greater than the volume of the cuboid.
+     * "Equal", if both objects have equal volume.
+     * Print the string without quotes.
+     * You can print each character of the output in either uppercase or lowercase.
+     * For example, the strings Cube, CUBE, cube, and CuBe are considered identical.
+     * 
+     * Constraints:
+     * 1 <= A, B, C, X <= 10
+     */
+    public static void solveCodeChefCABLE() {
+        Scanner scn = new Scanner(System.in);
+        long A = scn.nextLong(); 
+        long B = scn.nextLong(); 
+        long C = scn.nextLong(); 
+        long X = scn.nextLong(); 
+        long V1 = A * B * C;
+        long V2 = X * X * X;
+        if (V1 > V2)
+            System.out.println("Cuboid");
+        else if (V1 < V2)
+            System.out.println("Cube");
+        else
+            System.out.println("Equal");
+        scn.close();
+    }
     
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
