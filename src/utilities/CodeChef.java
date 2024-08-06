52da88c5292481dbdd692981650be711eb6238a6
Ismael Flores <iflores@circontrol.com>
2024-08-06 12:51:44 +0200

    feat: Add solution for CodeChef problem 'Binary String Cost' (BSCOST)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -8076,6 +8076,57 @@ public class CodeChef {
         }
         scn.close();
     }
+
+    /*
+     * Problem: Binary String Cost
+     * 
+     * According to the new tax scheme, a new tax called the binary string tax was introduced! 
+     * JJ has a binary string S of length N. According to the tax scheme, for every occurrence of 01 in S, 
+     * a tax of X rupees will be charged, while for every occurrence of 10 in S, a tax of Y rupees will be charged.
+     * For example, if X = 5, Y = 7 and S = 11010, then S has 1 occurrence of 01 and 2 occurrences of 10,
+     * so the tax charged = 5 * 1 + 7 * 2 = 19
+     * JJ can rearrange the string S in any way he wants. 
+     * He wants to minimize the amount of tax he has to pay. Can you help him do so?
+     * 
+     * Input Format:
+     * The first line contains a single integer T - the number of test cases. Then the test cases follow.
+     * The first line of each test case contains three integers N, X and Y - 
+     * the length of the binary string S, the tax charged on an occurrence of 01 and the tax charged on an occurrence of 10.
+     * The second line of each test case contains a binary string S of length N containing 0s and 1s only.
+     * 
+     * Output Format:
+     * For each test case, output the minimum amount of tax that JJ has to pay.
+     * 
+     * Constraints:
+     * 1 <= T <= 100
+     * 2 <= N <= 1000
+     * 1 <= X, Y <= 100
+     */
+    public static void solveCodeChefBSCOST() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong(); 
+        while (T-- > 0) {
+            long N = scn.nextLong();
+            long X = scn.nextLong();
+            long Y = scn.nextLong();
+            String S = scn.next();
+            long zeros = 0;
+            long ones = 0;
+            for (long i = 0; i < N; i++) {
+                if (S.charAt((int)i) == '0')
+                    zeros++;
+                else
+                    ones++;
+            }
+            if (zeros == 0 || ones == 0)
+                System.out.println(0);
+            else if (X <= Y)
+                System.out.println(X);
+            else
+                System.out.println(Y);
+        }
+        scn.close();
+    }
     
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
