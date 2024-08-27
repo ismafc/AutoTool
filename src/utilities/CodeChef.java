f42bd9684bbc22bd51224401cc6386e73b04b763
Ismael Flores <ismafc@gmail.com>
2024-08-27 15:46:32 +0200

    feat: Add solution for CodeChef problem 'Clearance Sale' (CLEARANCE)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -8197,6 +8197,30 @@ public class CodeChef {
         }
         scn.close();
     }
+
+    /*
+     * Problem: Clearance Sale
+     *
+     * Chef's favorite clothing store is holding a clearance sale: for every 2 t-shirts bought, you receive a third one for free!
+     * Chef bought and paid for X t-shirts from the shop. It is guaranteed that X is even.
+     * How many t-shirts did Chef get from the shop in total?
+     *
+     * Input Format:
+     * The first and only line of input contains a single even integer X, the number of t-shirts Chef paid for.
+     *
+     * Output Format:
+     * For each test case, output on a new line one integer: the total number of t-shirts Chef will get from the store.
+     *
+     * Constraints:
+     * 2 <= X <= 100
+     * X is even.
+    */
+    public static void solveCodeChefCLEARANCE() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong(); 
+        System.out.println(X + X / 2);
+        scn.close();
+    }
     
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
