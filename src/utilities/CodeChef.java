cf85f733a12d5b98bebed09c1411a64de76c36f0
Ismael Flores <ismafc@gmail.com>
2024-09-02 12:07:05 +0200

    feat: Add solution for CodeChef problem 'Approximate Answer' (P1149)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -8250,6 +8250,38 @@ public class CodeChef {
         System.out.println(X >= Y ? A : B);
         scn.close();
     }
+
+    /*
+     * Problem: Approximate Answer
+     *
+     * You are solving a problem whose correct answer is the integer Y.
+     * The answer you obtained, however, is the integer X.
+     * Your answer will be considered correct if the difference between X and Y is at most K.
+     * In other words, your answer is considered correct if and only if: |X - Y| <= K
+     * Given the values of X, Y, and K, determine whether your answer X is correct or not.
+     * Note that |X| denotes the absolute value of X.
+     * For example, |12∣ = 12, |−15| = 15 and |0| = 0.
+     *
+     * Input Format:
+     * The first and only line of input will contain three space-separated integers X, Y and K - 
+     * your answer, the correct answer, and the the maximum allowed difference, respectively.
+     *
+     * Output Format:
+     * Output the answer on a single line: "Yes" if your answer is considered correct, and "No" otherwise (without quotes).
+     * Each letter of the output may be printed in either uppercase or lowercase, 
+     * i.e, the strings NO, no, No, and nO will all be treated as equivalent.
+     *
+     * Constraints:
+     * 1 <= X, Y, K <= 20
+     */
+    public static void solveCodeChefP1149() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong(); 
+        long Y = scn.nextLong();
+        long K = scn.nextLong();
+        System.out.println(Math.abs(X - Y) <= K ? "Yes" : "No");
+        scn.close();
+    }
     
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
