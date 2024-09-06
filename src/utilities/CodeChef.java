1dcadd335728e1282ce926479123ce141d1a1d91
Ismael Flores <ismafc@gmail.com>
2024-09-06 11:57:17 +0200

    feat: Add solution for CodeChef problem 'IOI 2024' (IOI2024)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -8410,6 +8410,30 @@ public class CodeChef {
         }
         scn.close();
     }
+
+    /*
+     * Problem: IOI 2024
+     *
+     * IOI 2024 is being held in Egypt, starting from the 1-st of September and ending on the 8-th of September.
+     * Given a date X in September, output YES if IOI is ongoing then, otherwise NO.
+     *
+     * Input Format:
+     * The first and only line of input contains one integer X - the date in September.
+     *
+     * Output Format:
+     * Output either YES or NO, depending on whether IOI is ongoing on September X.
+     * Each character of the output may be printed in either lowercase or uppercase 
+     * - that is, the strings no, NO, No, and nO will all be treated as equivalent.
+     *
+     * Constraints:
+     * 1 <= X <= 30
+     */
+    public static void solveCodeChefIOI2024() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong();
+        System.out.println(X <= 8 ? "YES" : "NO");
+        scn.close();
+    }
     
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
