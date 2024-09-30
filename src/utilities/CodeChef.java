b212d3fecfa88f570396e6b938e9ebbc23c57a58
Ismael Flores <ismafc@gmail.com>
2024-09-30 15:57:43 +0200

    feat: Add solution for CodeChef problem 'AI is Coming' (AICOM)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -8637,6 +8637,34 @@ public class CodeChef {
     }
 
     /*
+     * Problem: AI is Coming
+     *
+     * You are given an assignment from your institute.
+     * Each assignment has a difficulty value between 1 and 100 (both inclusive).
+     * AI can solve an assignment if and only if the difficulty of the assignment is less than or equal to 60.
+     * Your current assignment has difficulty X.
+     * Is AI capable of solving your assignment?
+     * Print "YES" if it is, and "NO" if it isn't (without the quotes).
+     *
+     * Input Format:
+     * The first and only line of input will contain a single integer X, denoting the difficulty of your assignment.
+     *
+     * Output Format:
+     * If AI is capable of solving your assignment output "YES", otherwise print "NO" (without quotes).
+     * Each character of the output may be printed in either uppercase or lowercase, 
+     * i.e, the strings no, NO, No, and nO will all be treated as equivalent.
+     *
+     * Constraints:
+     * 1 <= X <= 100
+     */
+    public static void solveCodeChefAICOM() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong(); 
+        System.out.println(X <= 60 ? "YES" : "NO");
+        scn.close();
+    }
+    
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
