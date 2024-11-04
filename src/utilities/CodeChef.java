139ed3ba6275ac4eaee9334bc5e55fd21daea5b2
Ismael Flores <iflores@circontrol.com>
2024-11-04 16:44:33 +0100

    feat: Add solution for CodeChef problem 'Glass Prices' (GLPR)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -9123,6 +9123,38 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Glass Prices
+     * 
+     * Chef is buying spectacles, and is now deciding on which frame to purchase.
+     * Chef has narrowed his options down to two choices: 
+     * a plastic frame that costs X rupees, and a metal frame that costs Y rupees.
+     * Chef will buy the metal frame if and only if it costs at most twice the price of the plastic frame - 
+     * otherwise he will buy the plastic frame.
+     * Can you decide which frame Chef will buy?
+     * 
+     * Input Format:
+     * The first and only line of input will contain two integers X and Y - 
+     * the price of the plastic frame and the metal frame, respectively.
+     * 
+     * Output Format:
+     * For each test case, output on a new line the answer: the type of frame Chef will purchase.
+     * This is either the string "PLASTIC" or the string "METAL" (without quotes) depending on the outcome.
+     * Each letter of the output may be printed in either uppercase or lowercase - 
+     * for example, the strings METAL, MeTaL, metal, and mEtAL will all be treated as equivalent.
+     * 
+     * Constraints:
+     * 1 <= X <= 2000
+     * 1 <= Y <= 2000
+     */
+    public static void solveCodeChefGLPR() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong(); 
+        long Y = scn.nextLong();        
+        System.out.println(Y > 2 * X ? "PLASTIC" : "METAL");
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
