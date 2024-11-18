9da4ffcfbd993a7b39a2c4751a6b88ebe74eb458
Ismael Flores <iflores@circontrol.com>
2024-11-18 12:42:24 +0100

    feat: Add solution for CodeChef problem 'Icecream and Cone' (ICECONE)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -9259,6 +9259,30 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Icecream and Cone
+     * 
+     * Chef has X cones and Y scoops of ice cream. Each ice cream cone requires exactly one cone and one scoop of ice cream.
+     * Your task is to determine the maximum number of ice cream cones Chef can make with the available ingredients.
+     * 
+     * Input Format:
+     * Input will contain two integers X and Y - the number of cones and ice cream scoops, respectively.
+     * 
+     * Output Format:
+     * Output the maximum number of ice cream cones Chef can make.
+     * 
+     * Constraints:
+     * 1 <= X <= 100
+     * 1 <= Y <= 100
+     */
+    public static void solveCodeChefICECONE() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong(); 
+        long Y = scn.nextLong();        
+        System.out.println(Math.min(X, Y));
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
