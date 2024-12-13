93d5c0ff84a757dc2955223d7ddbe063023c638b
Ismael Flores <ismafc@gmail.com>
2024-12-13 11:32:18 +0100

    feat: Add solution for CodeChef problem 'Itz Simple' (SPC2025Q2)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -9960,6 +9960,63 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Itz Simple
+     * 
+     * Ved and Varun are both members of Shaastra’s CnL team this year. 
+     * While they are equally skilled in programming, they differ in height. 
+     * Ved’s height is K, and Varun’s height is P.
+     * Both want to watch a movie in OAT from outside the gate, without entering. 
+     * There are N chairs available outside OAT, each with a height represented as Ai​. 
+     * These chairs can be stacked on top of each other to create a platform, allowing them to stand on it for a better view of the movie.
+     * Tej, the OAT security officer, gives all the chairs to Varun, except for one - the tallest chair, which is given to Ved.
+     * Now, Tej wants to know who will have a better view of the movie. 
+     * If Ved has a better view, print Ved. 
+     * If Varun has a better view, print Varunn. 
+     * If both have the same view, print Equall.
+     * Note: A person is said to have a better view if they can get a better combined height by stacking their chairs and standing on top of them.
+     * 
+     * Input Format:
+     * The first line of input will contain a single integer T, denoting the number of test cases.
+     * Each test case consists of multiple lines of input.
+     * -> The first line of each test case contains three space-separated integers N, K and P - the number of chairs, Ved's height and Varun's height
+     * -> The next line contains N space-separated integers - A1, A2, ..., AN​.
+     * 
+     * Output Format:
+     * For each test case, output Ved if Ved will have a better view, Varun if Varun will have a better view, or Equal if both will have the same view.
+     * Each character of the strings can be printed in upper or lower case. VED, ved and vEd will all be accepted.
+     * 
+     * Constraints:
+     * 1 <= T <= 100
+     * 1 <= N <= 100
+     * 1 <= K, P <= 10^4
+     * 1 <= Ai <= 100
+     */
+    public static void solveCodeChefSPC2025Q2() {
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt(); 
+        while (T-- > 0) {
+            long N = scn.nextLong(); 
+            long K = scn.nextLong(); 
+            long P = scn.nextLong();
+            long tallest = 0;
+            long sum = 0;
+            for (long i = 0; i < N; i++) {
+                long Ai = scn.nextInt();
+                tallest = Math.max(Ai, tallest);
+                sum += Ai;
+            }
+            sum -= tallest;
+            if (K + tallest > P + sum)
+                System.out.println("Ved");
+            else if (K + tallest < P + sum)
+                System.out.println("Varun");
+            else
+                System.out.println("Equal");
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
