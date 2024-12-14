700a349722266e63e12edfffe9fc9ef3ceef743b
Ismael Flores <ismafc@gmail.com>
2024-12-14 19:32:33 +0100

    feat: Add solution for CodeChef problem 'Three Way Communications' (COMM3)

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -10017,6 +10017,59 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Three Way Communications
+     * 
+     * The Chef likes to stay in touch with his staff. 
+     * So, the Chef, the head server, and the sous-chef all carry two-way transceivers so they can stay in constant contact. 
+     * Of course, these transceivers have a limited range so if two are too far apart, they cannot communicate directly.
+     * The Chef invested in top-of-the-line transceivers which have a few advanced features. 
+     * One is that even if two people cannot talk directly because they are out of range, 
+     * if there is another transceiver that is close enough to both, 
+     * then the two transceivers can still communicate with each other using the third transceiver as an intermediate device.
+     * There has been a minor emergency in the Chef's restaurant and he needs to communicate with both the head server and the sous-chef right away. 
+     * Help the Chef determine if it is possible for all three people to communicate with each other, 
+     * even if two must communicate through the third because they are too far apart.
+     * 
+     * Input:
+     * The first line contains a single positive integer T <= 100 indicating the number of test cases to follow. 
+     * The first line of each test case contains a positive integer R <= 1000 
+     * indicating that two transceivers can communicate directly without an intermediate transceiver if they are at most R meters away from each other. 
+     * The remaining three lines of the test case describe the current locations of the Chef, the head server, and the sous-chef, respectively. 
+     * Each such line contains two integers X, Y (at most 10000 in absolute value) indicating that the respective person is located at position X, Y.
+     * 
+     * Output:
+     * For each test case you are to output a single line containing a single string. 
+     * If it is possible for all three to communicate then you should output "yes". Otherwise, you should output "no".
+     * To be clear, we say that two transceivers are close enough to communicate directly 
+     * if the length of the straight line connecting their X,Y coordinates is at most R.
+     */
+    public static void solveCodeChefCOMM3() {
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt(); 
+        while (T-- > 0) {
+            long R = scn.nextLong(); 
+            long X1 = scn.nextLong(); 
+            long Y1 = scn.nextLong(); 
+            long X2 = scn.nextLong(); 
+            long Y2 = scn.nextLong(); 
+            long X3 = scn.nextLong(); 
+            long Y3 = scn.nextLong(); 
+            long D12 = Math.abs(X1 - X2) * Math.abs(X1 - X2) + Math.abs(Y1 - Y2) * Math.abs(Y1 - Y2);
+            long D23 = Math.abs(X2 - X3) * Math.abs(X2 - X3) + Math.abs(Y2 - Y3) * Math.abs(Y2 - Y3);
+            long D13 = Math.abs(X1 - X3) * Math.abs(X1 - X3) + Math.abs(Y1 - Y3) * Math.abs(Y1 - Y3);
+            long visibles = 0;
+            if (D12 <= R * R)
+                visibles++;
+            if (D23 <= R * R)
+                visibles++;
+            if (D13 <= R * R)
+                visibles++;
+            System.out.println(visibles > 1 ? "yes" : "no");
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
