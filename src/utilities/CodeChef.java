09f8920332c96bce075a2f1ea835a89dda64ea9c
Ismael Flores <ismafc@gmail.com>
2025-02-01 09:39:57 +0100

    feat: Add solution for CodeChef problem 'Advitiya' (ADVITIYA). Implemented method to calculate minimum steps to convert a string to 'ADVITIYA'.

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -11069,6 +11069,54 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Advitiya
+     * 
+     * Toofan wanted to write the string "ADVITIYA", but accidentally wrote the 8-letter string S instead. 
+     * He wants to correct his mistake with the minimum number of steps.
+     * In one step, Toofan can choose an index i (1 <= i <= N), and change Si​ to the next letter in the alphabet.
+     * That is, change A -> B, B -> C, C -> D, and so on.
+     * The alphabet is considered cyclic, so Z can be changed to A.
+     * For example, if S equals "ZARAGOZA",
+     * Operating on the first index will turn S into "AARAGOZA".
+     * Operating on the second index will instead turn S into "ZBRAGOZA".
+     * Operating on the third index will instead turn S into "ZASAGOZA".
+     * And so on for the other indices.
+     * Find the minimum number of steps required to convert the string S into "ADVITIYA".
+     * 
+     * Input Format:
+     * The first line contains an integer T - the number of test cases.
+     * The next T lines each contain a string S consisting of exactly 8 uppercase English letters.
+     * 
+     * Output Format:
+     * For each test case, output a single integer on a new line - the minimum number of steps to convert S into "ADVITIYA".
+     * 
+     * Constraints:
+     * 1 <= T <= 1000
+     * S has length 8.
+     * Each character of S is an uppercase English letter.
+     */
+    public static void solveCodeChefADVITIYA() {
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt(); 
+        while (T-- > 0) {
+            String S = scn.next();
+            String ADVITIYA = "ADVITIYA";
+            long steps = 0;
+            int i = 0;
+            for (char c : S.toCharArray()) {
+                long d = (long)ADVITIYA.charAt(i) - (long)c;
+                if (d >= 0)
+                    steps += d;
+                else
+                    steps += 26 + d;
+                i++;
+            }
+            System.out.println(steps);
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
