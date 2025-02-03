2f0ee6bf7bdced899f06766b04a4a317cb274826
Ismael Flores <iflores@circontrol.com>
2025-02-03 14:45:37 +0100

    feat: Add solution for CodeChef problem 'Cut the Board' (CUTBOARD). Implemented method to compute the maximum number of steps to cut the chessboard without splitting it.

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -11117,6 +11117,42 @@ public class CodeChef {
     }
 
     /*
+     * Promlem: Cut the Board
+     * 
+     * Suzumo has a chessboard with N rows and M columns. 
+     * In one step, he can choose two cells of the chessboard which share a common edge 
+     * (that has not been cut yet) and cut this edge.
+     * Formally, the chessboard is split into two or more pieces 
+     * if it is possible to partition its cells into two non-empty subsets S1 and S2 
+     * (S1 ∩ S2 = ∅, |S1| + |S2| = N * M) such that there is no pair of cells c1, c2 (c1 ∈ S1, c2 ∈ S2) 
+     * which share a common edge that has not been cut.
+     * Suzumo does not want the board to split into two or more pieces. 
+     * Compute the maximum number of steps he can perform while satisfying this condition.
+     * 
+     * Input:
+     * The first line of the input contains a single integer T denoting the number of test cases. 
+     * The description of T test cases follows.
+     * The first and only line of each test case contains two space-separated integers N and M.
+     * 
+     * Output:
+     * For each test case, print a single line containing one integer - the maximum possible number of steps.
+     * 
+     * Constraints:
+     * 1 <= T <= 64
+     * 1 <= N, M <= 8
+     */
+    public static void solveCodeChefCUTBOARD() {
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt(); 
+        while (T-- > 0) {
+            Long N = scn.nextLong();
+            Long M = scn.nextLong();
+            System.out.println((N - 1) * (M - 1));
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
