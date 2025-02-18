075a05d44e1b3e9fa165de35b386704c6a9e7bc6
Ismael Flores <ismafc@gmail.com>
2025-02-18 20:03:33 +0100

    feat: Add solution for CodeChef problem 'Chef and Eid' (EID). Implemented method to find the minimum possible difference between the values of coins given to two children.

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -11650,6 +11650,53 @@ public class CodeChef {
         }
         scn.close();
     }
+
+    /*
+     * Problem: Chef and Eid
+     * 
+     * During Eid, it's a tradition that each father gives his kids money which they can spend on entertainment.
+     * Chef has N coins; let's denote the value of coin i by vi​. 
+     * Since today is Eid, Chef is going to give one coin to each of his two children. 
+     * He wants the absolute value of the difference between the values of coins given to the two children to be as small as possible, 
+     * so that he would be as fair as possible.
+     * Help Chef by telling him the minimum possible difference between the values of the coins given to the two children. 
+     * Of course, Chef cannot give the same coin to both children.
+     * 
+     * Input:
+     * The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+     * The first line of each test case contains a single integer N.
+     * The second line contains N space-separated integers v1, v2, ..., vN.
+     * 
+     * Output:
+     * For each test case, print a single line containing one integer - the minimum possible difference.
+     * 
+     * Constraints:
+     * 1 <= T <= 100
+     * 2 <= N <= 10^5
+     * the sum of N in all test cases does not exceed 5 * 10^5
+     * 1 <= vi <= 10^6 for each valid i
+     */
+    public static void solveCodeChefEID() {
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt(); 
+        while (T-- > 0) {
+            long N = scn.nextLong();
+            TreeSet<Long> tree = new TreeSet<>();
+            long minDiff = Long.MAX_VALUE;
+            for (long i = 0; i < N; i++) {
+                long Vi = scn.nextLong();
+                Long floor = tree.floor(Vi);
+                Long ceiling = tree.ceiling(Vi);
+                if (floor != null)
+                    minDiff = Math.min(minDiff, Vi - floor);
+                if (ceiling != null)
+                    minDiff = Math.min(minDiff, ceiling - Vi);
+                tree.add(Vi);
+            }
+            System.out.println(minDiff);
+        }
+        scn.close();
+    }
     
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
