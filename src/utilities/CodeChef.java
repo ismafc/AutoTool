4730cb556e14cf14945d9b89a19947c7f6ff8ca7
Ismael Flores <ismafc@gmail.com>
2025-02-20 14:35:05 +0100

    feat: Add solution for CodeChef problem 'Too Much Homework' (HWFIN). Implemented method to determine if Chef can achieve a full score based on completed and available questions.

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -11697,7 +11697,35 @@ public class CodeChef {
         }
         scn.close();
     }
-    
+
+    /*
+     * Problem: Too Much Homework!
+     * 
+     * Chef just realized he has a ton of homework due tomorrow!
+     * Chef has several worksheets of questions with him. Each worksheet has exactly Y questions in it.
+     * Chef only has the time to complete at most 10 worksheets before the submission deadline.
+     * Chef has previously answered X questions, and he needs to answer at least 100 questions in total to get a full score for the homework.
+     * Will Chef be able to get a full score?
+     * 
+     * Input Format:
+     * The only line of input will contain two space-separated integers X and Y - 
+     * the number of questions already answered by Chef, and the number of questions in each worksheet.
+     * 
+     * Output Format:
+     * For each test case, output the answer: "YES" (without quotes) if Chef can receive a full score for the homework, and "NO" (without quotes) otherwise.
+     * Each character of the output may be printed in either uppercase or lowercase, i.e. the strings No, NO, nO, and no will be treated as equivalent.
+     * 
+     * Constraints:
+     * 0 <= X <= 150
+     * 1 <= Y <= 10
+     */
+    public static void solveCodeChefHWFIN() {
+        Scanner scn = new Scanner(System.in);
+        long X = scn.nextLong(); 
+        long Y = scn.nextLong(); 
+        System.out.println((X + Y * 10) >= 100 ? "YES" : "NO");
+        scn.close();
+    }    
     /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
