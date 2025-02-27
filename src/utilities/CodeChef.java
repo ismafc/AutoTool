4e47190b06196317eff56e388f1484669c2ae786
Ismael Flores <ismafc@gmail.com>
2025-02-27 20:17:37 +0100

    feat: Add solution for CodeChef problem 'Chef and Glove' (CHEGLOVE). Implemented method to determine if Chef can wear the glove in front, back, both, or neither orientations based on finger and sheath lengths.

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -11918,6 +11918,69 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Chef and Glove
+     * 
+     * Winter has finally come to Chefland, so Chef bought a glove for herself. 
+     * Chef has a hand with N fingers (for the sake of simplicity, let us consider the thumb a finger too) numbered 1 through N 
+     * and the glove has N sheaths numbered 1 through N. Exactly one finger has to be inserted into each sheath. 
+     * You are given the lengths of Chef's fingers, as seen from left to right. 
+     * You are also given the lengths of the sheaths of the glove, from left to right, as seen from the front.
+     * Chef can wear the glove normally (the front side of the glove matching the front of Chef's hand), 
+     * or she can flip the glove and then insert her hand in it. In the first case, the first finger will go into the first sheath, 
+     * the second finger into the second one, and so on. 
+     * However, when she flips the glove, her first finger will go into the N-th sheath, 
+     * the second finger into the (N - 1)-th and so on — the i-th finger will go into the (N + 1 - i)-th sheath for each valid i. 
+     * Of course, for her to wear the glove comfortably, each finger's length should be less than or equal to the length of the sheath it goes into.
+     * Find out whether Chef can wear the glove by keeping its front side facing her, or if she can wear it by flipping it over and facing its back side. 
+     * If she can only wear the glove in the former way, output "front"; 
+     * if she can wear it only in the latter way, output "back". 
+     * If both front and back orientations can be used, output "both", and if she can't wear it either way, output "none".
+     * 
+     * Input:
+     * The first line of the input contains a single integer T denoting the number of test cases. The description of T test cases follows.
+     * The first line of each test case contains a single integer N denoting the number of Chef's fingers.
+     * The second line contains N space-separated integers L1, L2, ..., LN denoting the lengths of Chef's fingers.
+     * The third line contains N space-separated integers G1, G2, ..., GN, denoting the lengths of sheaths of the glove.
+     * 
+     * Output:
+     * For each test case, print a single line containing one of the strings "front", "back", "both", or "none".
+     * 
+     * Constraints:
+     * 1 <= T <= 10
+     * 1 <= N <= 10^5
+     * 1 <= Li <= 10^9 for each valid i
+     * 1 <= Gi <= 10^9 for each valid i
+     */
+    public static void solveCodeChefCHEGLOVE() {
+        Scanner scn = new Scanner(System.in);
+        long T = scn.nextLong();
+        while (T-- > 0) {
+            long N = scn.nextLong();
+            ArrayList<Long> Fi = new ArrayList<>();
+            for (long i = 0; i < N; i++)
+                Fi.add(scn.nextLong());
+            boolean front = true;
+            boolean back = true;
+            for (long i = 0; i < N; i++) {
+                long Gi = scn.nextLong();
+                if (Fi.get((int)i) > Gi)
+                    front = false;
+                if (Fi.get((int)N - 1 - (int)i) > Gi)
+                    back = false;
+            }
+            if (front && back)
+                System.out.println("both");
+            else if (front)
+                System.out.println("front");
+            else if (back)
+                System.out.println("back");
+            else
+                System.out.println("none");
+        }
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
