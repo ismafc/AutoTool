21acab75370a6217a3826a85835e4b9e383422be
Ismael Flores <ismafc@gmail.com>
2025-03-22 10:08:53 +0100

    feat: Add solution for CodeChef problem 'Food Balance' (FOODBAL). Implemented method to determine which dish Chef should choose based on the closest fat and protein balance.

--- src/utilities/CodeChef.java
+++ src/utilities/CodeChef.java
@@ -12446,6 +12446,48 @@ public class CodeChef {
     }
 
     /*
+     * Problem: Food Balance
+     * 
+     * Chef is preparing to cook his dinner.
+     * There are two dishes Chef can make. The first one contains F1 grams of fat and P1 grams of protein, 
+     * while the second contains F2 grams of fat and P2 grams of protein.
+     * Chef would like the quantity of fats and proteins he consumes to be as close to each other as possible, 
+     * i.e., the absolute difference between the amount of fats and proteins should be as small as possible.
+     * Help Chef by telling him which dish he'll choose; or if both dishes have the same difference.
+     * 
+     * Input Format:
+     * The first and only line of input contains four space-separated integers F1, P1, F2, and P2 - 
+     * the quantities of fat and protein in the first dish and second dish, respectively.
+     * 
+     * Output Format:
+     * Output a single string:
+     * "First" (without quotes) if Chef will choose the first dish.
+     * "Second" (without quotes) if Chef will choose the second dish.
+     * "Both" (without quotes) if both dishes are equivalent.
+     * Each character of the output may be in either uppercase or lowercase, 
+     * i.e. if the answer is Both, then any of the strings BOTH, both, Both, bOTh, and so on will be accepted.
+     * 
+     * Constraints:
+     * 1 <= F1, P1, F2, P2 <= 100
+     */
+    public static void solveCodeChefFOODBAL() {
+        Scanner scn = new Scanner(System.in);
+        long F1 = scn.nextLong();
+        long P1 = scn.nextLong();
+        long F2 = scn.nextLong();
+        long P2 = scn.nextLong();
+        long diff1 = Math.abs(F1 - P1);
+        long diff2 = Math.abs(F2 - P2);
+        if (diff1 == diff2)
+            System.out.println("Both");
+        else if (diff1 < diff2)
+            System.out.println("First");
+        else
+            System.out.println("Second");
+        scn.close();
+    }
+
+    /*
      * @brief: Method to show the CodeChef solved problems implemented.
      * It shows all procedures with name starting with "solveCodeChef".
      */
